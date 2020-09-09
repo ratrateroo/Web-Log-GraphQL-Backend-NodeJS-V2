d1a185882bef96ddab4c066140042418b8b217a6
Mark <ratrateroo@gmail.com>
2020-09-09 15:26:20 +0800

    added error messages for unsupported routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const HttpError = require('./models/http-error');
 const blogsRoutes = require('./routes/blogs-routes');
 
 const app = express();
@@ -8,6 +9,11 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/blogs', blogsRoutes);
+
+app.use((req, res, next) => {
+	const error = new HttpError('Could not find this route!', 404);
+	throw error;
+});
 
 app.use((error, req, res, next) => {
 	if (res.headerSent) {
