cf1f1aa7da1d5bcf3053a8c23537ed66b6c3d4a0
Mark <ratrateroo@gmail.com>
2020-09-11 23:39:52 +0800

    added express validator for blog routes

--- routes/blogs-routes.js
+++ routes/blogs-routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const blogsControllers = require('../controllers/blogs-controller');
 
-const usersControllers = require('../controllers/users-controller');
 const router = express.Router();
 
 const image = 'Images here';
@@ -11,7 +11,7 @@ router.get('/:bid', blogsControllers.getBlogById);
 
 router.get('/user/:uid', blogsControllers.getBlogsByUserId);
 
-router.post('/', blogsControllers.createBlog);
+router.post('/', [check('title').not().isEmpty()], blogsControllers.createBlog);
 
 router.patch('/:bid', blogsControllers.updateBlog);
 
