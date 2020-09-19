de1c8e3ebb686de6372229209d5eaf10a30f3b77
Mark <ratrateroo@gmail.com>
2020-09-19 23:35:10 +0800

    added user model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	username: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true, minlength: 6 },
+	image: { type: String, required: true },
+	firstname: { type: String, required: true },
+	middlename: { type: String, required: true },
+	lastname: { type: String, required: true },
+});
