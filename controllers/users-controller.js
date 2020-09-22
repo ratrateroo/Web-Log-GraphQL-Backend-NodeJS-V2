56f6afe71a9d22dbca4216882eae59c5a80dd891
Mark <ratrateroo@gmail.com>
2020-09-22 23:03:43 +0800

    updated get users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -30,8 +30,18 @@ let USERS = [
 	},
 ];
 
-const getUsers = (req, res, next) => {
-	res.json({ users: USERS });
+const getUsers = async (req, res, next) => {
+	let users;
+	try {
+		users = await User.find({}, '-password');
+	} catch (err) {
+		const error = new HttpError(
+			'Fetching users failed, please try again later.',
+			500
+		);
+		return next(error);
+	}
+	res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const getUserFriends = (req, res, next) => {
